fe190fdba87306e89455817d22ed4df1ce1d600e
Diego Lopez <lopezleondiego0@gmail.com>
2023-10-03 22:01:58 -0500

    Primer commit

---
+++ main.py
@@ -0,0 +1,67 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI(
+    title="App con FastAPI", version="0.0.1", description="Una API para aprender"
+)
+movies = [
+    {
+        "id": 1,
+        "title": "Avatar",
+        "overview": "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
+        "year": "2009",
+        "rating": 7.8,
+        "category": "Acción",
+    },
+    {
+        "id": 2,
+        "title": "Avatar",
+        "overview": "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
+        "year": "2009",
+        "rating": 7.8,
+        "category": "Acción",
+    },
+]
+
+
+@app.get("/", tags=["home"], response_class=HTMLResponse)
+def message():
+    return """
+        <html
+            <head>
+                <title>Web App con FastAPI</title>
+                </head>
+                <body>
+                    <h1>¡Hola a todos!</h1>
+                    <p>Este es un mensaje de ejemplo</p>
+                </body>
+        </html>
+"""
+
+
+@app.get("/movies/{id}", tags=["movies"])
+def get_movie(id: int):
+    try:
+        return [movie for movie in movies if movie["id"] == id][0]
+    except IndentationError:
+        return {"Error": "No hay pelicalas para mostrar"}
+
+
+@app.get("/items/{item_id}/sub/{item_id2}")
+def read_item(item_id: int, item_id2: str):
+    return {"item_id": item_id, "item_id2": item_id2}
+
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
