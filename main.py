111ef598dd91a91a584206c1bccc75403626ed69
DiegoLopez2549 <lopezleondiego0@gmail.com>
2023-11-03 21:14:21 -0500

    ..

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from fastapi import FastAPI, Body
+from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
-from pydantic import BaseModel
 
 app = FastAPI(
     title="App con FastAPI", version="0.0.1", description="Una API para aprender"
@@ -53,12 +52,9 @@ def read_item(item_id: int, item_id2: str):
     return {"item_id": item_id, "item_id2": item_id2}
 
 
-# Porque las operaciones de path son evaluadas en orden,
-# tienes que asegurarte de que el path para /users/me
-# sea declarado antes que el path para /users/{user_id}:
-# De otra manera el path para /users/{user_id}
-# coincidiría también con /users/me "pensando"
-# que está recibiendo el parámetro user_id con el valor "me".
+from fastapi import FastAPI
+
+app = FastAPI()
 
 
 @app.get("/users/me")
@@ -69,72 +65,3 @@ async def read_user_me():
 @app.get("/users/{user_id}")
 async def read_user(user_id: str):
     return {"user_id": user_id}
-
-
-# Parametro Query
-@app.get("/movies/", tags=["movies"])
-def get_movies_by_category(category: str, year: int):
-    return category
-
-
-"""@app.post("/movies/", tags=["movies"])
-def create_movie(
-    id: int = Body(default=1),
-    title: str = Body(default=0),
-    overview: str = Body(default=0),
-    year: int = Body(default=0),
-    rating: float = Body(default=0),
-    category: str = Body(default="text"),
-):
-    movies.append(
-        {
-            "id": id,
-            "title": title,
-            "overview": overview,
-            "year": year,
-            "rating": rating,
-            "category": category,
-        }
-    )
-    return movies
-"""
-
-
-# Otra forma de hacerlo...
-# Modelo de datos para representar información de películas.
-class Movies(BaseModel):
-    id: int
-    title: str
-    overview: str
-    year: int
-    rating: float
-    category: str
-
-
-@app.post("/movies/", tags=["movies"])
-def create_movie(movie: Movies = Body(...)):
-    movies.append(movie)
-    return movies
-
-
-# Editar una película
-@app.put("/movies/{id}", tags=["movies"])
-def update_movie(id: int, movie: Movies):
-    for item in movies:
-        if item["id"] == id:
-            item["title"] = movie.title
-            item["overview"] = movie.overview
-            item["year"] = movie.year
-            item["rating"] = movie.rating
-            item["category"] = movie.category
-    return movies
-
-
-# Eliminar una película
-@app.delete("/movies/{id}", tags=["movies"])
-def delete_movie(id: int):
-    for movie in movies:
-        if movie["id"] == id:
-            movies.remove(movie)
-            break
-    return movies
