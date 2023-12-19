934a5f4bfbe1b96b46f670338bd9f90d7d02843b
DiegoLopez2549 <lopezleondiego0@gmail.com>
2023-12-18 21:20:26 -0500

    pp

--- Backend/FastAPI/main.py
+++ Backend/FastAPI/main.py
@@ -5,3 +5,15 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/rutas")
+async def get_full_name(first_name: str, last_name: str):
+    full_name = first_name.title() + " " + last_name
+    return full_name
+
+print(get_full_name("Diego", "Lopez"))
+
+@app.get("/rutas2")
+async def get_name_whith_age(name: str, age: int):
+    name_whith_age = name + " tiene " + str(age)
+    return name_whith_age
